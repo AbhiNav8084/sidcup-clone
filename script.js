96fbfd00fd5e6db2be6be7038d38abea5a819c76
AbhiNav8084 <shubhamkumarsingh415@gmail.comm>
2024-10-02 18:22:45 +0530

    sidcup-clone

--- script.js
+++ script.js
@@ -1,10 +1,39 @@
+var crsr = document.querySelector("#cursor")
+var blurr = document.querySelector("#cursor-blur")
+
+document.addEventListener("mousemove",function(dets){
+    crsr.style.left = dets.x+"px"
+    crsr.style.top = dets.y+"px"
+    blurr.style.left = dets.x - 250 +"px"
+    blurr.style.top = dets.y - 250 +"px"
+})
+
+
+
 gsap.to("#nav",{
     backgroundColor : "black",
     duration : 0.5,
-    height : "120px",
+    height : "110px",
     scrollTrigger : {
         trigger : "#nav",
         scroller : "body",
-        markers: true
+        // markers: true,
+        start:"top -10%",
+        end : "top -11%",
+        scrub : 1
     }
 })
+
+gsap.to("#main",{
+    backgroundColor : "black",
+    scrollTrigger : {
+        trigger : "#main",
+        scroller : "body",
+        markers : true,
+        start : "top -25%",
+        end : "top -70%",
+        scrub : 2
+    }
+})
+
+
