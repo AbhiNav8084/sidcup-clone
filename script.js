a64de53dfe3d6d3c5e048f50eea0a9e094924c39
AbhiNav8084 <shubhamkumarsingh415@gmail.comm>
2024-10-03 14:42:01 +0530

    h

--- script.js
+++ script.js
@@ -2,12 +2,25 @@ var crsr = document.querySelector("#cursor")
 var blurr = document.querySelector("#cursor-blur")
 
 document.addEventListener("mousemove",function(dets){
-    crsr.style.left = dets.x+"px"
-    crsr.style.top = dets.y+"px"
+    crsr.style.left = dets.x +"px"
+    crsr.style.top = dets.y +"px"
     blurr.style.left = dets.x - 250 +"px"
     blurr.style.top = dets.y - 250 +"px"
 })
 
+var h4all = document.querySelectorAll("#nav h4")
+h4all.forEach(function(elem){
+    elem.addEventListener("mouseenter",function(){
+        crsr.style.scale = 3
+        crsr.style.border = "1px solid #fff"
+        crsr.style.backgroundColor = "transparent"
+    })
+    elem.addEventListener("mouseleave",function(){
+        crsr.style.scale = 1
+        crsr.style.border = "0px solid #95C11E"
+        crsr.style.backgroundColor = "#95C11E"
+    })
+})
 
 
 gsap.to("#nav",{
@@ -36,4 +49,63 @@ gsap.to("#main",{
     }
 })
 
+gsap.from("#about-us img,#about-us-in",{
+    y:70,
+    opacity:0,
+    duration:1,
+    scrollTrigger:{
+        trigger:"#about-us",
+        scroller:"body",
+        start:"top 70%",
+        end :"top 65% ",
+        scrub:1.5,
+    }
+})
 
+gsap.from("#cards-container ",{
+    scale:0.8,
+    duration:1,
+    stagger: 0.1,
+    scrollTrigger:{
+        trigger:"#cards-container",
+        scroller:"body",
+        start:"top 70%",
+        end :"top 65% ",
+        scrub:1
+    }
+})
+
+gsap.from("#colon1",{
+    y:-70,
+    x:-70,
+    scrollTrigger:{
+        trigger:"#colon1",
+        scroller: "body",
+        start:"top 55%",
+        end: "top 45%",
+        scrub: 4,
+    }
+})
+
+gsap.from("#colon2",{
+    y:70,
+    x:70,
+    scrollTrigger:{
+        trigger:"#colon1",
+        scroller: "body",
+        start:"top 55%",
+        end: "top 45%",
+        scrub: 4,
+    },
+});
+
+gsap.from("#page4 h1",{
+    y:50,
+    scrollTrigger:{
+        trigger:"#page4 h1",
+        scroller: "body",
+        start:"top 75%",
+        end: "top 70%",
+        scrub: 3,
+    },
+});
